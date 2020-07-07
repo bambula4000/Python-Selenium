a4d3bc035725196732f615946c095c664442ca64
oleksandrd <bambula4000@gmail.com>
2020-07-07 16:27:07 +0300

    Add test file with implicit wait
    Added test file with implicit wait example

---
+++ ImplicitWait.py
@@ -0,0 +1,18 @@
+from selenium import webdriver
+
+driver = webdriver.Chrome("C://Users//Grom//Documents/chromedriver.exe")
+driver.implicitly_wait(10)  # wait for any element, max time 10 seconds
+driver.get("https://www.phptravels.net/admin")
+
+assert "Administator Login" in driver.title
+
+driver.find_element_by_name("email").send_keys("admin@phptravels.com")
+driver.find_element_by_name("password").send_keys("demoadmin")
+
+driver.find_element_by_class_name(
+    "btn.btn-primary.btn-block.ladda-button.fadeIn.animated.btn-lg"
+).click()
+
+driver.find_element_by_id('select_all')
+
+driver.quit()
