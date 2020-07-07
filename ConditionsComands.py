99f1033de36d1e695b5983704a51b52aa5698d39
oleksandrd <bambula4000@gmail.com>
2020-07-07 15:08:26 +0300

    Add test file with conditions commands
    Added test file with conditions commands examples

---
+++ ConditionsComands.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from time import sleep
+
+driver = webdriver.Chrome("C://Users//Grom//Documents//chromedriver.exe")
+
+driver.get("https://www.phptravels.net/admin")
+
+email_elem = driver.find_element_by_name("email")
+print(email_elem.is_enabled())
+print(email_elem.is_displayed())
+
+password_elem = driver.find_element_by_name("password")
+print(password_elem.is_enabled())
+print(password_elem.is_displayed())
+
+email_elem.send_keys("admin@phptravels.com")
+password_elem.send_keys("demoadmin")
+
+driver.find_element_by_class_name(
+    "btn.btn-primary.btn-block.ladda-button.fadeIn.animated.btn-lg"
+).click()
+sleep(5)
+
+button = driver.find_element_by_id('select_all')
+print(button.is_selected())
+
+driver.quit()
+
