b60a2c5e29566ee5f1bf6952b06fb2e6789cdba2
oleksandrd <bambula4000@gmail.com>
2020-07-09 10:27:50 +0300

    Add test file for input fields
    Added tests example for input fields

---
+++ InputBoxes.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome("C://Users//Grom//Documents//chromedriver.exe")
+
+driver.get("https://accounts.ukr.net/registration")
+
+input_boxes = driver.find_elements(By.CLASS_NAME, "form__section")
+print(len(input_boxes))
+
+driver.find_element(By.ID, "id-login").send_keys('ababagalamaga')
+driver.find_element(By.ID, "id-password").send_keys("123Qssww")
+driver.find_element(By.ID, "id-password-repeat").send_keys("123Qssww")
+
+driver.find_element(By.ID, "id-first-name").send_keys("Valeriy")
+driver.find_element(
+    By.CSS_SELECTOR,
+    "/html/body/div/div/main/form/section[3]/section[1]/div/div[2]/input"
+).send_keys("Leontiev")
