231d60632e1fb7b24368c2fa20da0b4d8f8fea88
oleksandrd <bambula4000@gmail.com>
2020-07-09 12:20:22 +0300

    Add comment

--- Tools/Locators.py
+++ Tools/Locators.py
@@ -1,2 +1,3 @@
+#  registration form selectors (url: "https://accounts.ukr.net/registration")
 month_of_year = "/html/body/div/div/main/form/section[3]/section[2]/div/section/ul/li[{}]"
 month_expand = "input-select__target"
