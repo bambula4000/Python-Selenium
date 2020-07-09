8670c61da2d328078698900b31ae8d9477af6aeb
oleksandrd <bambula4000@gmail.com>
2020-07-09 12:18:42 +0300

    Add variable with path to month expand
    Added variable to avoid hardcode

--- Dropdown.py
+++ Dropdown.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from Tools.Path import path_to_driver
 from Tools.Urls import registration_form
-from Tools.Locators import month_of_year
+from Tools.Locators import month_of_year, month_expand
 
 
 driver = webdriver.Chrome(path_to_driver)
@@ -11,5 +11,5 @@ driver.get(registration_form)
 
 month_number = 3
 
-element = driver.find_element(By.CLASS_NAME, "input-select__target").click()
+element = driver.find_element(By.CLASS_NAME, month_expand).click()
 driver.find_element(By.XPATH, month_of_year.format(month_number)).click()
--- Tools/Locators.py
+++ Tools/Locators.py
@@ -1,1 +1,2 @@
 month_of_year = "/html/body/div/div/main/form/section[3]/section[2]/div/section/ul/li[{}]"
+month_expand = "input-select__target"
